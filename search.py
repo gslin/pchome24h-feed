71f41c65a194acaeec8108360632ac2b59f41a77
Gea-Suan Lin <gslin@gslin.org>
2017-11-01 12:16:37 +0800

    Get JSON search result.

--- search.py
+++ search.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import requests
 import sys
 
 def procedure(keyword):
-    pass
+    url = 'https://ecshweb.pchome.com.tw/search/v3.3/all/results?q=%s&page=1&sort=new/dc' % (keyword)
+
+    r = requests.get(url);
 
 if __name__ == '__main__':
     procedure(sys.argv[1])
